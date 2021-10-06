4a351a689cd19cc5066c2f8eb6e56838b8c6595f
demirug <pm.antondolia@gmail.com>
2021-10-06 04:35:12 +0300

    Removed info page

--- seven_sins_store/urls.py
+++ seven_sins_store/urls.py
@@ -5,21 +5,15 @@ from django.conf.urls.static import static
 
 from orders.views import cart, TrackView
 from products.views import ProductDetailView, ProductListView
-from seven_sins_store.views import info
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('info/', info, name='info'),
-
     path('authorization/', include(('authorization.urls', 'authorization'))),
-
     path('<slug:slug>', ProductDetailView.as_view(), name='view'),
     path('', ProductListView.as_view(), name='home'),
-
     path('orders/', include(('orders.urls', 'orders'))),
     path('cart/', cart, name='cart'),
     path('track/<int:pk>', TrackView.as_view(), name='track'),
-
     path('manager/', include(('manager.urls', 'manager'))),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- seven_sins_store/views.py
+++ seven_sins_store/views.py
@@ -1,6 +1,1 @@
-from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
-
-
-def info(request: WSGIRequest):
-    return render(request, 'info.html')
