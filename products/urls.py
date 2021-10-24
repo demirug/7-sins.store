b530547ad731886fcc746be9912bf9fd7056ff37
demirug <64663730+demirug@users.noreply.github.com>
2021-10-24 03:47:00 +0300

    Removed useless url file

--- products/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import *
-
-urlpatterns = [
-    path('<slug:slug>', ProductDetailView.as_view(), name='view'),
-    path('', ProductListView.as_view(), name='home'),
-]
