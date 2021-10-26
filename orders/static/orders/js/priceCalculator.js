8272909bbd72451fb5be144fb6c56f1007e2ca33
demirug <64663730+demirug@users.noreply.github.com>
2021-10-26 07:40:07 +0300

    Fixed: In cart user can't set incorrect value in count input field

--- orders/static/orders/js/priceCalculator.js
+++ orders/static/orders/js/priceCalculator.js
@@ -19,7 +19,16 @@ function updateData() {
      let totalPrice = 0
      $('.product').each(function () {
         let price = $(this).find('#price').text();
-        let count =  $(this).find('#count').val();
+        let obj =  $(this).find('#count')
+        if(Number(obj.val()) > Number(obj.attr('max'))) {
+            obj.val(obj.attr('max'));
+        }
+
+        if(Number(obj.val()) < Number(obj.attr('min'))) {
+            obj.val(obj.attr('min'));
+        }
+
+        let count = obj.val();
         $(this).find('#total').text(price * count);
         totalPrice += price * count
     })
