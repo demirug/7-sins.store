50509b48272fa223f9387ea907ec9e32ffa4e4bb
demirug <64663730+demirug@users.noreply.github.com>
2021-10-27 04:23:32 +0300

    Added getting email host/port from env

--- seven_sins_store/settings.py
+++ seven_sins_store/settings.py
@@ -147,10 +147,9 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
-
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_HOST_USER = os.getenv("EMAIL_LOGIN")
 EMAIL_HOST_PASSWORD = os.getenv("PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("FROM_EMAIL")
