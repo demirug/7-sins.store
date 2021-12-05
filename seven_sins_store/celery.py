0594ae71337b0bd3c824f72345fe2063bd5cf607
demirug <pm.antondolia@gmail.com>
2021-12-05 11:05:57 +0200

    Registered schedule celery task

--- seven_sins_store/celery.py
+++ seven_sins_store/celery.py
@@ -3,6 +3,8 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
+from celery.schedules import crontab
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seven_sins_store.settings')
 
 app = Celery('7-sins-store')
@@ -15,3 +17,10 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'send-admin-email-task': {
+        'task': 'manager.tasks.send_admin_email_task',
+        'schedule': crontab(0, 0, '*', '*', '*')
+    },
+}
